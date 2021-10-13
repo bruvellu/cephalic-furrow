75534c49609485e28b1af0afcb84210eea2fcbe6
Bruno C. Vellutini <cosserme@mpi-cbg.de>
2021-10-13 17:28:43 +0200

    Removes samples in the wrong orientation

--- 1-analyses/fold-depth/0-load.sh
+++ 1-analyses/fold-depth/0-load.sh
@@ -4,18 +4,15 @@ BASEDIR=/projects/fileserver/projects/vellutini/cephalic_furrow
 
 # 2020-09-24_btd-gap_dorsal
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E1.tif ./0-data/
-ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E2.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E3.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E4.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E5.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E6.tif ./0-data/
-ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E7.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E8.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E9.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E10.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E11.tif ./0-data/
 ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E12.tif ./0-data/
-ln -s ${BASEDIR}/2020-09-24_btd-gap_dorsal/btd-gap_dorsal1_z3_t51s_E13.tif ./0-data/
 
 # 2020-10-13_btd-gap_dorsal
 ln -s ${BASEDIR}/2020-10-13_btd-gap_dorsal/btd-gap_dorsal_1_z1.5_t51s_E1.tif ./0-data/
@@ -48,7 +45,6 @@ ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E9.tif ./0-d
 ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E10.tif ./0-data/
 ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E11.tif ./0-data/
 ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E12.tif ./0-data/
-ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E13.tif ./0-data/
 ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E17.tif ./0-data/
 ln -s ${BASEDIR}/2020-11-03_eve-gap_dorsal/eve-gap_dorsal_1_z3_t52s_E19.tif ./0-data/
 
